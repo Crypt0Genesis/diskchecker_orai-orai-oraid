c8a0a1d23fe4c71540fb73859efb8dda92917a42
Crypto Genesis <107373370+Crypt0Genesis@users.noreply.github.com>
2024-05-10 00:23:16 +0100

    Update check_disk_script.sh

--- check_disk_script.sh
+++ check_disk_script.sh
@@ -3,11 +3,11 @@
 # Check disk usage for /dev/ partitions
 dev_disk_usage=$(df -h | grep '/dev/' | awk '{ print $5 }' | sed 's/%//')
 
-# Check disk usage specifically for /dev/sda2 partition
-sda2_disk_usage=$(df -h | grep '/dev/sda2' | awk '{ print $5 }' | sed 's/%//')
+# Check disk usage specifically for /dev/sda* partition
+sda_disk_usage=$(df -h | grep '/dev/sda*' | awk '{ print $5 }' | sed 's/%//')
 
 # Combine the values, assuming that both partitions may exist
-disk_usage=$(echo "$dev_disk_usage $sda2_disk_usage" | tr '\n' ' ')
+disk_usage=$(echo "$dev_disk_usage $sda_disk_usage" | tr '\n' ' ')
 
 # Check if disk usage is greater than or equal to 80%
 for usage in $disk_usage; do
